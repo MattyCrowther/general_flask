b055c6c2412588c1adce01b8e01b4bcbd887c573
mcrowther <mattycrowther1@hotmail.co.uk>
2024-02-07 10:34:41 +0000

    Initial commit

--- app/utility/form_handlers.py
+++ app/utility/form_handlers.py
@@ -1,33 +1,18 @@
 import os
 from datetime import datetime
-from urllib.parse import urlparse
 from werkzeug.utils import secure_filename
 
-def handle_upload(form,sess_dir,file_type=None):
+def handle_upload(form,sess_dir):
     file_data = form.upload.data
-    if form.file_type.data == "SBOL":
-        suffix = ".xml"
-    else:
-        suffix = ".gbk"
     filename = file_data.filename
     filename = secure_filename(filename)
     secure_fn = os.path.join(sess_dir,filename)
     file_data.save(secure_fn)
-    if hasattr(form,"graph_name") and form.graph_name.data != "":
-        gn = form.graph_name.data 
-        secure_fn_n = os.path.join(sess_dir,gn+suffix)
-        os.rename(secure_fn,secure_fn_n) 
-        secure_fn = secure_fn_n
-    else: 
-        gn = filename.split(".")[0]
+    gn = filename.split(".")[0]
     return secure_fn,gn
 
 def handle_paste(form,sess_dir):
-    if form.file_type.data == "SBOL":
-        suffix = ".xml"
-    else:
-        suffix = ".gbk"
-
+    suffix = ".txt"
     if form.graph_name.data != "":
         gn = form.graph_name.data 
     else: 
@@ -36,27 +21,3 @@ def handle_paste(form,sess_dir):
     with open(filename,"a+") as f:
         f.write(form.paste.data)
     return filename,gn
-
-def handle_synbiohub(form,sess_dir,connector):
-    identifier = form.pmid.data
-    if _uri_validator(identifier):
-        identifier = identifier.split("/")[-2]
-
-    if form.graph_name.data != "":
-        gn = form.graph_name.data 
-    else: 
-        gn = identifier
-
-    filename = os.path.join(sess_dir,gn + ".xml")
-    try:
-        connector.get(identifier,output=filename)
-    except ValueError:
-        return None
-    return filename,gn
-
-def _uri_validator(x):
-    try:
-        result = urlparse(x)
-        return all([result.scheme, result.netloc])
-    except:
-        return False
